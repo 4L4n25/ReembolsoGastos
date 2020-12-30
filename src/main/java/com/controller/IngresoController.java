ca9d883fcb1fd59fe9f15b466a3bad82cb22a513
4L4n25 <73363089+4L4n25@users.noreply.github.com>
2020-12-30 13:21:33 -0600

    .

--- src/main/java/com/controller/IngresoController.java
+++ src/main/java/com/controller/IngresoController.java
@@ -9,8 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.json.simple.JSONObject;
-
-import javax.json.JsonArray;
 import java.util.List;
 
 @RestController
